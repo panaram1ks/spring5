0356a5005e9b88a40d5b8dfe6f3adf66cae453b5
parom <E.Parominskiy@isida.by>
2023-08-24 12:30:22 +0300

    XML-based Constructor Injection

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -8,8 +8,8 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("application.xml");
 //        System.out.println(context.getBean(ConnectionPool.class));
-        ConnectionPool pool2 = context.getBean("pool2", ConnectionPool.class);
-        System.out.println(pool2);
+        ConnectionPool pool1 = context.getBean("pool1", ConnectionPool.class);
+        System.out.println(pool1);
 
     }
 
