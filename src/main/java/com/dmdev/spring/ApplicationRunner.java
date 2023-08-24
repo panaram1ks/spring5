e427777db3157ac123044a25cbc9f5f08a3fec40
parom <E.Parominskiy@isida.by>
2023-08-24 12:07:48 +0300

    XML-based Configuration context(interface BeanFactory, ApplicationContext)

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -1,26 +1,16 @@
 package com.dmdev.spring;
 
 import com.dmdev.spring.database.pool.ConnectionPool;
-import com.dmdev.spring.database.repository.CompanyRepository;
-import com.dmdev.spring.database.repository.UserRepository;
-import com.dmdev.spring.ioc.Container;
-import com.dmdev.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        Container container = new Container();
+        var context = new ClassPathXmlApplicationContext("application.xml");
+//        System.out.println(context.getBean(ConnectionPool.class));
+        ConnectionPool pool2 = context.getBean("pool2", ConnectionPool.class);
+        System.out.println(pool2);
 
-//        ConnectionPool connectionPool = new ConnectionPool();
-//        UserRepository userRepository = new UserRepository(connectionPool);
-//        CompanyRepository companyRepository = new CompanyRepository(connectionPool);
-//        UserService userService = new UserService(userRepository, companyRepository);
-//        // We are ready to use UserService and all it functionality!
-
-//        ConnectionPool connectionPool = container.get(ConnectionPool.class);
-//        UserRepository userRepository = container.get(UserRepository.class);
-//        CompanyRepository companyRepository = container.get(CompanyRepository.class);
-        UserService userService = container.get(UserService.class);
     }
 
 }
