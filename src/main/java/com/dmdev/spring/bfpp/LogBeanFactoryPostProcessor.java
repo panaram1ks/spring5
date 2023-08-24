9affb00f3e2b13d84df85b3e419165b768e8634a
parom <E.Parominskiy@isida.by>
2023-08-24 16:32:14 +0300

    Custom BeanFactoryPostProcessor order interface Ordered does't change default order

--- src/main/java/com/dmdev/spring/bfpp/LogBeanFactoryPostProcessor.java
+++ src/main/java/com/dmdev/spring/bfpp/LogBeanFactoryPostProcessor.java
@@ -3,13 +3,18 @@ package com.dmdev.spring.bfpp;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
 
 
-public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, Ordered {
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
         System.out.println();
     }
 
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
 }
