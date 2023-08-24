9b9451a49b867937a3cbb62f385183d52396143c
parom <E.Parominskiy@isida.by>
2023-08-24 12:50:34 +0300

    check debug

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -1,6 +1,7 @@
 package com.dmdev.spring;
 
 import com.dmdev.spring.database.pool.ConnectionPool;
+import com.dmdev.spring.database.repository.CompanyRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
@@ -10,6 +11,9 @@ public class ApplicationRunner {
 //        System.out.println(context.getBean(ConnectionPool.class));
         ConnectionPool pool1 = context.getBean("pool1", ConnectionPool.class);
         System.out.println(pool1);
+
+        CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
 
     }
 
