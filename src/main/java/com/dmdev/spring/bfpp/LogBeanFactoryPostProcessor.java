c042c7e7abaa930ec4b273260aca3029192af511
parom <E.Parominskiy@isida.by>
2023-08-24 16:52:06 +0300

    Custom BeanFactoryPostProcessor order interface PriorityOrdered can perform before default beans !

--- src/main/java/com/dmdev/spring/bfpp/LogBeanFactoryPostProcessor.java
+++ src/main/java/com/dmdev/spring/bfpp/LogBeanFactoryPostProcessor.java
@@ -4,9 +4,10 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
 
 
-public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, Ordered {
+public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
