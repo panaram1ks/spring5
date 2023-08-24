549c2dfe871c1cfff972399f9eeb0a7506e4af72
parom <E.Parominskiy@isida.by>
2023-08-24 16:20:33 +0300

    (BFPP) BeanFactoryPostProcessor

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -2,11 +2,22 @@ package com.dmdev.spring;
 
 import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.database.repository.CompanyRepository;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.Serializable;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
+        String value = "anytext";
+        boolean assignableFrom = CharSequence.class.isAssignableFrom(value.getClass());
+        System.out.println(assignableFrom);
+        boolean assignableFrom1 = BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass());
+        System.out.println(assignableFrom1);
+        boolean assignableFrom2 = Serializable.class.isAssignableFrom(value.getClass());
+        System.out.println(assignableFrom2);
+
         CompanyRepository companyRepository;
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
 //        System.out.println(context.getBean(ConnectionPool.class));
