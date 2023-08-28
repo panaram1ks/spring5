6fcd2d46e7d22b2659d6adfe158400d4abff91ae
parom <E.Parominskiy@isida.by>
2023-08-28 17:14:11 +0300

    Java-base Create bean use 1) @Bean annotation, 2) As Field in Configuration.class

--- src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -1,7 +1,11 @@
 package com.dmdev.spring.config;
 
+import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.database.repository.CrudRepository;
+import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -20,5 +24,16 @@ import static org.springframework.context.annotation.ComponentScan.*;
 )
 public class ApplicationConfiguration {
 
+        // we can create bean as a field of ApplicationConfiguration.class cause it is Component
 
+        @Bean("pool2")
+        @Scope(BeanDefinition.SCOPE_SINGLETON)
+        public ConnectionPool pool2(@Value("${db.username}") String username) {
+                return new ConnectionPool(username, 20);
+        }
+
+        @Bean
+        public UserRepository userRepository2(ConnectionPool pool2){
+                return new UserRepository(pool2);
+        }
 }
