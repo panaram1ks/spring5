1a9a03fa2d3c7d4469096f51579f5be7c3edb191
parom <E.Parominskiy@isida.by>
2023-08-29 11:20:51 +0300

    @ConfigurationProperties

--- src/main/java/com/dmdev/spring/config/DatabaseProperties.java
+++ src/main/java/com/dmdev/spring/config/DatabaseProperties.java
@@ -2,30 +2,26 @@ package com.dmdev.spring.config;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
-@Data
-@NoArgsConstructor
 @ConfigurationProperties(prefix = "db")
 //@Component //or use @ConfigurationPropertiesScan on mane class
-public class DatabaseProperties {
-    private String username;
-    private String password;
-    private String driver;
-    private String url;
-    private String hosts;
-    private PoolProperties pool;
-    private List<PoolProperties> pools;
-    private Map<String, Object> properties;
+public record DatabaseProperties(String username,
+                                 String password,
+                                 String driver,
+                                 String url,
+                                 String hosts,
+                                 PoolProperties pool,
+                                 List<PoolProperties> pools,
+                                 Map<String, Object> properties) {
+    public static record PoolProperties(Integer size,
+                                        Integer timeout) {
 
-    @Data
-    @NoArgsConstructor
-    public static class PoolProperties {
-        private Integer size;
-        private Integer timeout;
     }
 }
