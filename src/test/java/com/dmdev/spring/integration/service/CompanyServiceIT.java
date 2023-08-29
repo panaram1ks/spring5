d359a05e07fa1f3b663c1b46862b96d04a791d76
parom <E.Parominskiy@isida.by>
2023-08-29 15:02:45 +0300

    Integration tests use YAML file and spring.properties

--- src/test/java/com/dmdev/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/com/dmdev/spring/integration/service/CompanyServiceIT.java
@@ -1,32 +1,43 @@
 package com.dmdev.spring.integration.service;
 
 
+import com.dmdev.spring.config.DatabaseProperties;
 import com.dmdev.spring.dto.CompanyReadDto;
 import com.dmdev.spring.service.CompanyService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
 
+import java.util.Date;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-@SpringBootTest//(classes = ApplicationRunner.class) // (classes = ApplicationRunner.class)-optional, this annotation find main class itself
+//@SpringBootTest//(classes = ApplicationRunner.class) // (classes = ApplicationRunner.class)-optional, this annotation find main class itself
 //@ExtendWith(SpringExtension.class)
 //@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class) // initializers = ConfigDataApplicationContextInitializer.class need to get properties form YAML file
+@IT
+@RequiredArgsConstructor
+//@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL) // we can use it from spring.properties file
 public class CompanyServiceIT {
     public static final Integer COMPANY_ID = 1;
-    @Autowired
-    private CompanyService companyService;
+     private final CompanyService companyService;
+
+    private final DatabaseProperties databaseProperties;
 
     @Test
-    void findById(){
+    void findById() {
         Optional<CompanyReadDto> actualResult = companyService.findById(COMPANY_ID);
+
         assertTrue(actualResult.isPresent());
+
         var expectedResult = new CompanyReadDto(COMPANY_ID);
-        actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));;
+        actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
+        ;
     }
 
 }
