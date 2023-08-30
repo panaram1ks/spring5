36b91af63030de6a8ef9efabb6876d361faefd9f
parom <E.Parominskiy@isida.by>
2023-08-30 14:39:19 +0300

    Special Parameters

--- src/main/java/com/dmdev/spring/database/repository/UserRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/UserRepository.java
@@ -5,13 +5,16 @@ import com.dmdev.spring.database.entity.User;
 import com.dmdev.spring.database.pool.ConnectionPool;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
@@ -25,5 +28,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("update User  u set u.role = :role where u.id in (:ids)")
     int updateRole(@Param("role") Role role, @Param("ids") Long... ids);
+
+    Optional<User> findTopByOrderByIdDesc();
+
+    List<User> findTop3ByBirthDateBeforeOrderByBirthDateDesc(LocalDate birthDate);
+    List<User> findTop3ByBirthDateBefore(LocalDate birthDate, Sort sort);
 
 }
--- src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTestIT.java
+++ src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTestIT.java
@@ -5,10 +5,15 @@ import com.dmdev.spring.database.entity.User;
 import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Sort;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @IT
 @RequiredArgsConstructor
@@ -17,21 +22,46 @@ class UserRepositoryTestIT {
     private final UserRepository userRepository;
 
     @Test
+    void specialParameters(){
+        Sort.TypedSort<User> sortBy = Sort.sort(User.class);
+        Sort sort = sortBy.by(User::getFirstname).and(sortBy.by(User::getLastname));
+        // safe method create sort object use fields without hardcoding string field names
+        List<User> allUsers = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), sort.descending());
+        assertThat(allUsers).hasSize(3);
+    }
+
+    @Test
+    void checkSort() {
+        Sort sortById = Sort.by("firstname").and(Sort.by("lastname"));
+        List<User> allUsers = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), sortById.descending());
+        assertThat(allUsers).hasSize(3);
+    }
+
+    @Test
+    void checkFirstTop() {
+        List<User> allUsers = userRepository.findTop3ByBirthDateBeforeOrderByBirthDateDesc(LocalDate.now());
+        assertThat(allUsers).hasSize(3);
+        Optional<User> topUser = userRepository.findTopByOrderByIdDesc();
+        assertTrue(topUser.isPresent());
+        topUser.ifPresent(user -> assertEquals(5l, user.getId()));
+    }
+
+    @Test
     void checkQueries() {
 //        List<User> users = userRepository.findAllBy("%a%", "%ov%");
         List<User> users = userRepository.findAllBy("a", "ov");
-        Assertions.assertThat(users).hasSize(3);
+        assertThat(users).hasSize(3);
     }
 
     @Test
     void checkUpdate() {
         User ivan = userRepository.getById(1l);
-        org.junit.jupiter.api.Assertions.assertSame(Role.ADMIN, ivan.getRole());
+        assertSame(Role.ADMIN, ivan.getRole());
 
         int resultCount = userRepository.updateRole(Role.USER, 1l, 5l);
-        org.junit.jupiter.api.Assertions.assertEquals(resultCount, 2);
+        assertEquals(resultCount, 2);
 
         User theSameIvan = userRepository.getById(1l);
-        org.junit.jupiter.api.Assertions.assertSame(Role.USER, theSameIvan.getRole());
+        assertSame(Role.USER, theSameIvan.getRole());
     }
 }
