6e8faf8b3fff419f2825746cf627f7fa103578e2
parom <E.Parominskiy@isida.by>
2023-08-30 08:46:46 +0300

    Hibernate Entities mapping @EntityScan(packages or modules where lay entities)

---
+++ src/main/java/com/dmdev/spring/database/entity/Chat.java
@@ -0,0 +1,33 @@
+package com.dmdev.spring.database.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "chat")
+public class Chat implements BaseEntity<Long>{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "chat")
+    private List<UserChat> userChats = new ArrayList<>();
+}
