0342b7fdbfb89deaaa2f9abf9e9c32ade0360f5e
parom <E.Parominskiy@isida.by>
2023-08-30 16:16:27 +0300

    Projection native with Calculation fields in Projection

--- src/main/java/com/dmdev/spring/dto/PersonalInfo2.java
+++ src/main/java/com/dmdev/spring/dto/PersonalInfo2.java
@@ -1,6 +1,8 @@
 package com.dmdev.spring.dto;
 
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.time.LocalDate;
 
 public interface PersonalInfo2 {
@@ -8,6 +10,9 @@ public interface PersonalInfo2 {
     String getFirstname();
     String getLastname();
     String getBirthDate();
+
+    @Value("#{target.firstname + ' ' + target.lastname}")
+    String getFullName();
 
 
 }
