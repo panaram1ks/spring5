cbba854d670eedb83ab5c8600260ad6ccda2566b
parom <E.Parominskiy@isida.by>
2023-09-01 16:46:31 +0300

    MVC Testing

---
+++ src/test/java/com/dmdev/spring/integration/http/controller/UserControllerTest.java
@@ -0,0 +1,58 @@
+package com.dmdev.spring.integration.http.controller;
+
+import com.dmdev.spring.integration.IntegrationTestBase;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static com.dmdev.spring.dto.UserCreateEditDto.Fields.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
+class UserControllerTest extends IntegrationTestBase {
+
+    private final MockMvc mockMvc;
+
+    @Test
+    void findAll() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(view().name("user/users"))
+                .andExpect(model().attributeExists("users"));
+//                .andExpect(model().attribute("users", IsCollectionWithSize.hasSize(5)));
+    }
+
+    @Test
+    void testFindAll() {
+    }
+
+    @Test
+    void create() throws Exception {
+        mockMvc.perform(post("/users")
+                .param(username, "test@gmail.com")
+                .param(firstname, "Test")
+                .param(lastname, "TestTest")
+                .param(role, "ADMIN")
+                .param(companyId, "1")
+                .param(birthDate, "2000-01-01")
+        )
+                .andExpectAll(
+                        status().is3xxRedirection(),
+//                        redirectedUrlPattern("/users/*") OR
+                          redirectedUrlPattern("/users/{\\d+}")
+                );
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
