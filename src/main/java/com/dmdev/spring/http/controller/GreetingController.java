8b7d48fc067ccbef1fe6c18df5a445d0130b5d49
parom <E.Parominskiy@isida.by>
2023-09-01 11:55:21 +0300

    Model

--- src/main/java/com/dmdev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package com.dmdev.spring.http.controller;
 
 
+import com.dmdev.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,29 +11,49 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/v1/")
+@SessionAttributes({"user"}) // sessionScope
 public class GreetingController {
 
-
-    @GetMapping("/hello/{id}")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
-                              @RequestParam(value = "age", required = false) Integer age,
-                              @RequestHeader("accept") String accept,
-                              @CookieValue("JSESSIONID") String JSESSIONID,
-                              @PathVariable("id") Integer id) {
-//        @RequestParam(value = "age", required = false) Integer age // get parameter from request and auto-convert it to necessary Type // spring way
-//        @RequestHeader("accept") String accept  // spring way
-
-//        String age = request.getParameter("age"); // old way
-//        String accept = request.getHeader("accept"); // old way
-//        Cookie[] cookies = request.getCookies(); // old way
-
+    @GetMapping("/hello")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
+//        request.getSession().setAttribute(); // sessionScope
+//        request.setAttribute(); // requestScope
         modelAndView.setViewName("greeting/hello");
+        modelAndView.addObject("user", new UserReadDto(1l, "Ivan"));
 
-        return modelAndView;  //                 http://localhost:8080/api/v1/hello/29?age=18
+        return modelAndView;
     }
 
+//    @GetMapping("/hello")
+//    public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request) {
+//
+//        modelAndView.setViewName("greeting/hello");
+//        return modelAndView;
+//    }
+
+
+//    @GetMapping("/hello/{id}")
+//    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+//                              @RequestParam(value = "age", required = false) Integer age,
+//                              @RequestHeader("accept") String accept,
+//                              @CookieValue("JSESSIONID") String JSESSIONID,
+//                              @PathVariable("id") Integer id) {
+////        @RequestParam(value = "age", required = false) Integer age // get parameter from request and auto-convert it to necessary Type // spring way
+////        @RequestHeader("accept") String accept  // spring way
+//
+////        String age = request.getParameter("age"); // old way
+////        String accept = request.getHeader("accept"); // old way
+////        Cookie[] cookies = request.getCookies(); // old way
+//
+//        modelAndView.setViewName("greeting/hello");
+//
+//        return modelAndView;  //                 http://localhost:8080/api/v1/hello/29?age=18
+//    }
+
     @GetMapping("/bye")
-    public ModelAndView bye(ModelAndView modelAndView) {
+    public ModelAndView bye(@SessionAttribute("user") UserReadDto user, // request.getSession().getAttribute("user");
+                            ModelAndView modelAndView,
+                            HttpServletRequest request) {
         modelAndView.setViewName("greeting/bye");
 
         return modelAndView;
