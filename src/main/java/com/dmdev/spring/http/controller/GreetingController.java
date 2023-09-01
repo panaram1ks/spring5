5219f3780ce27e551cc601ea889e1b47bd7a9f43
parom <E.Parominskiy@isida.by>
2023-09-01 11:33:11 +0300

    Parameters, Headers, Cookies

--- src/main/java/com/dmdev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -2,21 +2,33 @@ package com.dmdev.spring.http.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/v1/")
 public class GreetingController {
 
 
-    @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView) {
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+                              @RequestParam(value = "age", required = false) Integer age,
+                              @RequestHeader("accept") String accept,
+                              @CookieValue("JSESSIONID") String JSESSIONID,
+                              @PathVariable("id") Integer id) {
+//        @RequestParam(value = "age", required = false) Integer age // get parameter from request and auto-convert it to necessary Type // spring way
+//        @RequestHeader("accept") String accept  // spring way
+
+//        String age = request.getParameter("age"); // old way
+//        String accept = request.getHeader("accept"); // old way
+//        Cookie[] cookies = request.getCookies(); // old way
+
         modelAndView.setViewName("greeting/hello");
 
-        return modelAndView;
+        return modelAndView;  //                 http://localhost:8080/api/v1/hello/29?age=18
     }
 
     @GetMapping("/bye")
