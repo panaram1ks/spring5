81c5b99f31deaeb2f33c962b58f76eadfd90f45c
evgen <parom.evgen@mail.ru>
2023-09-02 14:43:00 +0300

    CRUD layer (update)

--- src/main/java/com/dmdev/spring/service/CompanyService.java
+++ src/main/java/com/dmdev/spring/service/CompanyService.java
@@ -1,30 +1,38 @@
 package com.dmdev.spring.service;
 
 import com.dmdev.spring.database.repository.CompanyRepository;
-import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.spring.dto.CompanyReadDto;
 import com.dmdev.spring.listener.entity.AccessType;
 import com.dmdev.spring.listener.entity.EntityEvent;
+import com.dmdev.spring.mapper.CompanyReadMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CompanyService {
 
     private final CompanyRepository companyRepository;
     private final ApplicationEventPublisher eventPublisher;
+    private final CompanyReadMapper companyReadMapper;
 
-    @Transactional
+
     public Optional<CompanyReadDto> findById(Integer id){
         return companyRepository.findById(id).map(entity -> {
             // TODO: send event
             eventPublisher.publishEvent(new EntityEvent(entity, AccessType.READ));
-            return new CompanyReadDto(1, null);
+            return companyReadMapper.map(entity);
         });
+    }
+
+    public List<CompanyReadDto> findAll(){
+       return companyRepository.findAll().stream()
+               .map(companyReadMapper::map).toList();
     }
 }
